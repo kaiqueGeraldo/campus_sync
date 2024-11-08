12a5854ca88821f83baadb8f0e46e21afe3ece85
Kaique Santos <47215831841@escola927.edu>
2024-11-08 17:27:10 -0300

    add text input para a tela de cadastro

---
+++ lib/src/views/components/custom_input_text_cadastro.dart
@@ -0,0 +1,45 @@
+import 'package:campus_sync/src/models/colors/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class CustomInputTextCadastro extends StatelessWidget {
+  final TextEditingController controller;
+  final String labelText;
+  final String? Function(String?)? validator;
+  final int? maxLength;
+  final TextInputType keyboardType;
+
+  const CustomInputTextCadastro({
+    required this.controller,
+    required this.labelText,
+    required this.keyboardType,
+    this.validator,
+    this.maxLength,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      validator: validator,
+      maxLengthEnforcement: MaxLengthEnforcement.none,
+      maxLength: maxLength,
+      keyboardType: keyboardType,
+      decoration: InputDecoration(
+        label: Text(labelText),
+        floatingLabelStyle:
+            const TextStyle(color: AppColors.backgroundBlueColor),
+        border: const OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+          borderSide: BorderSide(color: Colors.black12),
+        ),
+        focusedBorder: const OutlineInputBorder(
+          borderSide: BorderSide(color: AppColors.backgroundBlueColor),
+          borderRadius: BorderRadius.all(Radius.circular(12)),
+        ),
+        counterText: '',
+      ),
+    );
+  }
+}
